2cf679f032260882f1d4b36b8f7d01a44c599ac3
Mark SATO <mark.sato1111@gmail.com>
2018-07-05 16:22:49 +0900

    福利厚生費

--- app/Controller/MemberProfilesController.php
+++ app/Controller/MemberProfilesController.php
@@ -1608,6 +1608,7 @@ class MemberProfilesController extends AppController{
                                     'conditions' => array('MonthlySalary.association_id' => $id, 'MonthlySalary.style' => $style, 'MonthlySalary.working_month' => $date)
                                 ));
                             }
+                            debug($monthly_salary);
                             # 配列きりかえ
                             if($style=="part"){
                                 $part_arr[$id] = $monthly_salary['MonthlySalary']['fee'];
