ec8a6698759c896f33c0a37491596f9b1d3bf171
Mark SATO <mark.sato1111@gmail.com>
2019-06-25 11:48:19 +0900

    excel

--- app/Controller/SalesController.php
+++ app/Controller/SalesController.php
@@ -327,7 +327,6 @@ class SalesController extends AppController{
 						$expenses = $this->Expense->find('all', array(
 							'conditions' => array('Expense.location_id' => $location['Location']['id'], 'Expense.working_day' => $working_day)
 						));
-						debug($expenses);exit;
 						if($expenses!=null){
 							#種類別累計計算
 							$expense_arr_two = array();
