d19a1eb2fa8b25457e4df09820730967ac89b2b9
Mark SATO <mark.sato1111@gmail.com>
2021-05-16 14:49:14 +0900

    latest updates

--- app/Controller/MemberProfilesController.php
+++ app/Controller/MemberProfilesController.php
@@ -1223,7 +1223,7 @@ class MemberProfilesController extends AppController{
             $sheet = $obj->getActiveSheet();
             $sheet->setTitle(date('Y年m月', strtotime($date)));
             # 税抜に金額修正する行
-            $num_arr = [15,16,17,18,19,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,42,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61];
+            $num_arr = [15,16,17,18,19,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,42,46,47,48,49,50,51,52,53,55,56,57,58,59,60,61];
             foreach($data as $d){
                 $arr = explode(',',$d);
                 $num = $arr[0];
